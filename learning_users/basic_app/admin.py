3d51e595f1eaec239ebd9e29a17f4bdf3a156ca0
Marian Munteanu <marian.munteanu@metrosystems.net>
2020-12-13 11:53:26 +0200

    my first commit on this project in GIT

---
+++ learning_users/basic_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from basic_app.models import UserProfileInfo
+# Register your models here. This is in order to see them in admin page. You need to imgrate them after you register here
+admin.site.register(UserProfileInfo)
